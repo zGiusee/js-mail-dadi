70f4bd64d9550fe3c807f26196a6f12c10d1b448
zGiusee <giusebelforte@gmail.com>
2023-11-24 10:31:28 +0100

    Class correction

--- js/script.js
+++ js/script.js
@@ -22,56 +22,60 @@ email_btn.addEventListener("click", function(){
     
     }
 
+    // INDICO DOVE SI TROVA IL BOTTONE NEL DOCUMENTO
+    let dice_btn = document.getElementById("dice_btn");
+
+    dice_btn.style.display = "none";
+
     // ESEGUO IL CONTROLLO DEI DATI
     if(check){
         message = "Complimenti, l'email che hai inserito è corretta! Ora puoi lanciare i dadi!"
         // console.log("L'email che hai inserito è corretta. Ora puoi lanciare i dadi!")
 
-    // ESERCIZIO NUM 2 CONCATENATO CON LA VERIFICA DELL'EMAIL
+        // ESERCIZIO NUM 2 CONCATENATO CON LA VERIFICA DELL'EMAIL
 
-    // INDICO DOVE SI TROVA IL BOTTONE NEL DOCUMENTO
-    let dice_btn = document.getElementById("dice_btn");
+        dice_btn.style.display = "block";
 
-    // CREO LA FUNZIONE DEL BOTTONE CHE LANCI I DADI
-    dice_btn.addEventListener("click", function(){
+        // CREO LA FUNZIONE DEL BOTTONE CHE LANCI I DADI
+        dice_btn.addEventListener("click", function(){
 
-    let message;
+            let message;
 
-    // CREO I GENERATORI DI NUMERI CASUALI
-    let dado1 = Math.random() * (6 - 1) + 1 ;
-    dado1 = dado1.toFixed(0);
+            // CREO I GENERATORI DI NUMERI CASUALI
+            let dado1 = Math.random() * (6 - 1) + 1 ;
+            dado1 = dado1.toFixed(0);
 
-    let dado2 = Math.random() * (6 - 1) + 1 ;
-    dado2 = dado2.toFixed(0);
+            let dado2 = Math.random() * (6 - 1) + 1 ;
+            dado2 = dado2.toFixed(0);
 
-    document.getElementById("umano").innerHTML = `Tu: ${dado2}`;
-    document.getElementById("computer").innerHTML = ` Computer: ${dado1}`;
+            document.getElementById("umano").innerHTML = `Tu: ${dado2}`;
+            document.getElementById("computer").innerHTML = ` Computer: ${dado1}`;
 
-    // FACCIO IN MODO DI STAMPARE IL RISULTATO SULLA CONSOLE
-    // console.log(dado1);
-    // console.log(dado2);
+            // FACCIO IN MODO DI STAMPARE IL RISULTATO SULLA CONSOLE
+            // console.log(dado1);
+            // console.log(dado2);
 
-    // CREO LE VARIABILI CHE AVRANNO COME VALORE I NUMERI GENERATI
-    let computer = dado1;
-    let umano = dado2; 
+            // CREO LE VARIABILI CHE AVRANNO COME VALORE I NUMERI GENERATI
+            let computer = dado1;
+            let umano = dado2; 
 
-    // ESEGUO LE CONDIZIONI E IL MESSAGIO INFORMATIVO SU CHI HA VINTO
-    if(umano < computer){
-        // console.log("Il Computer ha vinto!")
-        message = "Il computer ha vinto!";
-    }
-    else if(umano > computer){
-        message = "L'umano ha vinto!";
-        // console.log("L'umano ha vinto!")
-    }
-    else if(umano == computer){
-        // console.log("Pareggio!")
-        message = "Pareggio!";
-    }
+            // ESEGUO LE CONDIZIONI E IL MESSAGIO INFORMATIVO SU CHI HA VINTO
+            if(umano < computer){
+                // console.log("Il Computer ha vinto!")
+                message = "Il computer ha vinto!";
+            }
+            else if(umano > computer){
+                message = "L'umano ha vinto!";
+                // console.log("L'umano ha vinto!")
+            }
+            else if(umano == computer){
+                // console.log("Pareggio!")
+                message = "Pareggio!";
+            }
 
-    document.getElementById("dice_winner").innerHTML = message;
+            document.getElementById("dice_winner").innerHTML = message;
 
-})
+       })
     }
     else{
         message = "L'email che hai inserito non è corretta. Non puoi lanciare i dadi."
